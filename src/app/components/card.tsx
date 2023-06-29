1165c6579e7354f921da5b838ce94a07a7deebe3
Wallys Ferreira <wallysfsilva@gmail.com>
2023-06-28 23:35:59 -0300

    começar pagina inicial

---
+++ src/app/components/card.tsx
@@ -0,0 +1,8 @@
+export default function Card() {
+  return (
+    <div className='card verticalcenter-spacearound'>
+      <p>Encontramos alguns estúdios e profissionais na sua área.</p>
+      <input placeholder="Estilo ou Localização" />
+    </div>
+  );
+}
