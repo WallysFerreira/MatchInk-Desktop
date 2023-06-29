fc3a2b79ea35531d1ac5980609c5756a2ef4acf2
Wallys Ferreira <wallysfsilva@gmail.com>
2023-06-29 00:34:56 -0300

    adicionar imagem no background

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,6 +1,8 @@
+import styles from './card.module.css';
+
 export default function Card() {
   return (
-    <div className='card verticalcenter-spacearound'>
+    <div className={'card' + ' ' + 'verticalcenter-spacearound' + ' ' + styles.card}>
       <p>Encontramos alguns estúdios e profissionais na sua área.</p>
       <input placeholder="Estilo ou Localização" />
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 import Card from "./components/card";
+import Image from "next/image";
+import styles from './page.module.css';
 
 export default function Page() {
   return (
-    <Card />
+    <section className={styles.container}>
+      <Image
+        className={styles.img}
+        src='/logo.png'
+        alt='Logo'
+        height={300}
+        width={300}
+      />
+      <Card />
+  </section>
   );
 }
